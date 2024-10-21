75e4d8457a5c620ba5c29436572d0600aaedc55e
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-10-21 20:36:01 +0800

    Update ZenRock.sh

--- ZenRock.sh
+++ ZenRock.sh
@@ -308,24 +308,27 @@ function set_operator_config() {
         return
     fi
 
+    # 设置变量
+    EIGEN_OPERATOR_CONFIG="$HOME/.zrchain/sidecar/eigen_operator_config.yaml"
     read -p "请输入测试网 Holesky 终端点: " TESTNET_HOLESKY_ENDPOINT
-    read -p "请输入主网终端点: " MAINNET_ENDPOINT
-    read -p "请输入测试网 Holesky RPC URL (默认使用测试网终端点): " ETH_RPC_URL
-    ETH_RPC_URL=${ETH_RPC_URL:-$TESTNET_HOLESKY_ENDPOINT}  # 如果用户没有输入，则使用 TESTNET_HOLESKY_ENDPOINT
-    read -p "请输入测试网 Holesky WebSocket URL: " ETH_WS_URL
-
+    MAINNET_ENDPOINT="YOUR_ETH_MAINNET_ENDPOINT"  # 这里可以根据需要设置
     OPERATOR_VALIDATOR_ADDRESS_TBD=$(zenrockd keys show wallet --bech val -a)
     OPERATOR_ADDRESS_TBU=$ecdsa_address
-    EIGEN_OPERATOR_CONFIG="$HOME/.zrchain/sidecar/eigen_operator_config.yaml"
+    ETH_RPC_URL=$TESTNET_HOLESKY_ENDPOINT  # 设置为与 TESTNET_HOLESKY_ENDPOINT 一致
+    read -p "请输入测试网 Holesky WebSocket URL: " ETH_WS_URL
     ECDSA_KEY_PATH=$ecdsa_output_file
     BLS_KEY_PATH=$bls_output_file
 
+    # 复制初始配置文件
     cp $HOME/zenrock-validators/configs/eigen_operator_config.yaml $HOME/.zrchain/sidecar/
     cp $HOME/zenrock-validators/configs/config.yaml $HOME/.zrchain/sidecar/
 
+    # 替换 config.yaml 中的变量
     sed -i "s|EIGEN_OPERATOR_CONFIG|$EIGEN_OPERATOR_CONFIG|g" "$HOME/.zrchain/sidecar/config.yaml"
     sed -i "s|TESTNET_HOLESKY_ENDPOINT|$TESTNET_HOLESKY_ENDPOINT|g" "$HOME/.zrchain/sidecar/config.yaml"
     sed -i "s|MAINNET_ENDPOINT|$MAINNET_ENDPOINT|g" "$HOME/.zrchain/sidecar/config.yaml"
+
+    # Replace variables in eigen_operator_config.yaml
     sed -i "s|OPERATOR_VALIDATOR_ADDRESS_TBD|$OPERATOR_VALIDATOR_ADDRESS_TBD|g" "$HOME/.zrchain/sidecar/eigen_operator_config.yaml"
     sed -i "s|OPERATOR_ADDRESS_TBU|$OPERATOR_ADDRESS_TBU|g" "$HOME/.zrchain/sidecar/eigen_operator_config.yaml"
     sed -i "s|ETH_RPC_URL|$ETH_RPC_URL|g" "$HOME/.zrchain/sidecar/eigen_operator_config.yaml"
@@ -350,7 +353,6 @@ LimitNOFILE=65535
 Environment="OPERATOR_BLS_KEY_PASSWORD=$key_pass"
 Environment="OPERATOR_ECDSA_KEY_PASSWORD=$key_pass"
 Environment="SIDECAR_CONFIG_FILE=$HOME/.zrchain/sidecar/config.yaml"
-Environment="BLS_PRIVATE_KEY_PATH=/root/.zrchain/sidecar/keys/bls.key.json"
 
 [Install]
 WantedBy=multi-user.target
